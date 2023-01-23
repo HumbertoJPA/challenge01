245069da3bb820600c67026f42b5190d4943e1b5
Humberto <parkerjavi@gmail.com>
2023-01-23 11:30:27 -0600

    actualizacion3

--- script.js
+++ script.js
@@ -8,62 +8,95 @@ const botonCopiar = document.querySelector(".botoncopiar");
 const imgBuscando = document.getElementById("imgbuscando");
 const msgNoHayTexto = document.getElementById("msgnohaytexto");
 
-function analizarMensaje(){
-  var mensaje1 = areaTexto.value;
-  var mensaje2 = areaEncriptado.value;
-  var caracteres = "abcdefghijklmnñopqrstuvwxyz \n";
-  var mensajeErroneo1= "";
-  var mensajeErroneo2= "";
-  var validador = true;
+function soloLetras(e) {
+    var key = e.keyCode || e.which,
+      tecla = String.fromCharCode(key).toLowerCase(),
+      letras = " abcdefghijklmnñopqrstuvwxyz",
+      especiales = [8, 37, 39, 46],
+      tecla_especial = false;
 
-  for(var caracter of mensaje1){
-    if(!caracteres.includes(caracter)){
-      mensajeErroneo1 = "ERROR"
+    for (var i in especiales) {
+      if (key == especiales[i]) {
+        tecla_especial = true;
+        break;
+      }
+    }
+
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+      return false;
     }
   }
-
-  for(var caracter of mensaje2){
-    if(!caracteres.includes(caracter)){
-      mensajeErroneo2 = "ERROR"
-    }
-  }
-
-  if (mensajeErroneo1 == "ERROR" || mensajeErroneo2 == "ERROR"){
-    validador = false;
-    alert("Ingrese carácteres válidos por favor");
-  }
-
-  return validador;
-}
 
 function encriptar(){
-  if(analizarMensaje() == false) return;
 
-  var mensaje = areaTexto.value;
-  var mensajeEncriptado = mensaje.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");
 
+  var nombre = areaTexto.value;
+
+  let data = []
+  var count = nombre.length
+  for(let i=0; i<count ;i++){
+      if(nombre[i]=="e"){
+          data.push('enter')
+      }else if(nombre[i]=='i'){
+          data.push('imes')
+      }else if(nombre[i]=='a'){
+          data.push('ai')
+      }else if(nombre[i]=='o'){
+          data.push('ober')
+      }else if(nombre[i]=='u'){
+          data.push('ufat')
+      }else{
+          data.push(nombre[i])
+      }
+  }
+
+  let frase = data.join("")
   areaEncriptado.style.display = "block";
   botonCopiar.style.display = "block";
 
   imgBuscando.hidden = true;  
   msgNoHayTexto.hidden = true;  
 
-  areaEncriptado.value = mensajeEncriptado;
+  areaEncriptado.value = frase;
 }
 
 function desencriptar(){
-  if(analizarMensaje() == false) return;
 
-  var mensajeEncriptado = areaTexto.value;
-  var mensajeDesencriptado = mensajeEncriptado.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");
+    var nombre = areaTexto.value;
+    let data = []
+    var count = nombre.length
+    console.log(nombre.length)
+    for(let i=0; i<count ;i++){
+        if(nombre[i]=="e"&&nombre[i+1]=="n"&&nombre[i+2]=="t"&&nombre[i+3]=="e"){
 
-  areaEncriptado.style.display = "block";
-  botonCopiar.style.display = "block";
+            data.push('e')
+            i=i+4
+        }else if(nombre[i]=='i'&&nombre[i+1]=="m"&&nombre[i+2]=="e"&&nombre[i+3]=="s"){
+            data.push('i')
+            i=i+3
+        }else if(nombre[i]=='a'&&nombre[i+1]=='i'){
+            data.push('a')
+            i=i+1
+        }else if(nombre[i]=='o'&&nombre[i+1]=='b'&&nombre[i+2]=='e'&&nombre[i+3]=='r'){
+            data.push('o')
+            i=i+3
+        }else if(nombre[i]=='u'&& nombre[i+1]=='f'&&nombre[i+2]=='a'&&nombre[i+3]=='t'){
+            data.push('u')
+            i=i+3
+        }else{
+            data.push(nombre[i])
+        }
+       
+    }
+    let frase = data.join("")
 
-  imgBuscando.hidden = true;  
-  msgNoHayTexto.hidden = true;  
-  
-  areaEncriptado.value = mensajeDesencriptado;
+    areaEncriptado.style.display = "block";
+    botonCopiar.style.display = "block";
+
+    imgBuscando.hidden = true;  
+    msgNoHayTexto.hidden = true;  
+    
+    areaEncriptado.value = frase;
 }
 
 function copiar(){
